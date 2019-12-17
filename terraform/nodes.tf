092d5f4c4f1d530df3aae3c66f0c1ab123ac652c
Ragni kumari <ragni085@gmail.com>
2019-12-18 00:35:03 +0530

    some change

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -20,3 +20,8 @@ resource "aws_instance" "node2" {
     Name = "Node2"
   }
 }
+
+resource "local_file" "inventory" {
+  filename = "../hosts-inventory"
+  content = "${aws_instance.node1.public_ip}\n${aws_instance.node2.public_ip}"
+}

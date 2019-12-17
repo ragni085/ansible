656a24b4b13d58b6d9aa780185e5a7d9005a90e7
Ragni kumari <ragni085@gmail.com>
2019-12-17 22:54:12 +0530

    Adding new file

---
+++ terraform/nodes.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "node1" {
+  ami = "ami-00c4b877294e28e09"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-001fb8d815f619f1c"]
+  tags {
+    Name = "Node1"
+  }
+}
+
+
+resource "aws_instance" "node2" {
+  ami = "ami-00c4b877294e28e09"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-001fb8d815f619f1c"]
+  tags {
+    Name = "Node2"
+  }
+}

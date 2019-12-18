3b527101465e310e94ef3c18ad08d6889720f242
Ragni kumari <ragni085@gmail.com>
2019-12-18 16:06:43 +0530

    some change

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "node1" {
 
 
 resource "aws_instance" "node2" {
-  ami = "ami-00c4b877294e28e09"
+  ami = "ami-099363826f6c9d6b4"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-001fb8d815f619f1c"]
   tags = {

04be55c1c305412dd6caa280a81344466db508fe
Ragni kumari <ragni085@gmail.com>
2019-12-21 00:02:55 +0530

    some change

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "node1" {
   ami = "ami-0c98a6bf9be7285b5"
   instance_type = "t2.micro"
+  key_name = "devops"
   vpc_security_group_ids = ["sg-0255375ce75e2cc8d"]
   tags = {
     Name = "Node1"
@@ -15,6 +16,7 @@ resource "aws_instance" "node1" {
 resource "aws_instance" "node2" {
   ami = "ami-0c98a6bf9be7285b5"
   instance_type = "t2.micro"
+  key_name = "devops"
   vpc_security_group_ids = ["sg-0255375ce75e2cc8d"]
   tags = {
     Name = "Node2"

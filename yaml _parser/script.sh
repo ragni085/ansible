11d55c7704487c99377cb1f018291156ecfbc404
Ragni kumari <ragni085@gmail.com>
2019-12-21 15:06:17 +0530

    Adding new file

---
+++ yaml _parser/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#if [! -f "$1"]; then
+# echo "Given file doesnot exist"
+# exit 1
+#fi
+
+echo -e "Hello XYZ, \n\tMy name TRAINER_NAME and I am a COURSE
+-NAME trainer.\nWe are starting a new batch from Date and timing is TIMING. If you are interested"

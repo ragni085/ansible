7e9b6a7e992e8c5e1fc5e8e7f3e6aef24b0fbd06
Ragni kumari <ragni085@gmail.com>
2019-12-21 15:12:59 +0530

    some change

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -13,18 +13,18 @@ resource "aws_instance" "node1" {
 }
 
 
-resource "aws_instance" "node2" {
-  ami = "ami-0c98a6bf9be7285b5"
-  instance_type = "t2.micro"
-  key_name = "devops"
-  vpc_security_group_ids = ["sg-0255375ce75e2cc8d"]
-  tags = {
-    Name = "Node2"
-  }
-}
+//resource "aws_instance" "node2" {
+  //ami = "ami-0c98a6bf9be7285b5"
+  //instance_type = "t2.micro"
+  //key_name = "devops"
+  //vpc_security_group_ids = ["sg-0255375ce75e2cc8d"]
+  //tags = {
+    //Name = "Node2"
+  //}
+//}
 
 resource "local_file" "inventory" {
   filename = "../hosts-inventory"
-  content  = "${aws_instance.node1.public_ip}\n${aws_instance.node2.public_ip}"
+  content  = "${aws_instance.node1.public_ip}"
 }
 

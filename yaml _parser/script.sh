b432b8e76fb81ec59de50d58526413058a8f9aac
Ragni kumari <ragni085@gmail.com>
2019-12-22 10:50:46 +0530

    some change

--- yaml _parser/script.sh
+++ yaml _parser/script.sh
@@ -5,5 +5,13 @@ if [! -f "$1"]; then
  exit 1
 fi
 
-echo -e "Hello XYZ,\n\tMy name TRAINER_NAME and I am a COURSE_NAME 
+i=0
+STUDENT_COUNT=$(batch43.yml | yq . | jq '.studentDetails[].name' | wc -l)
+NAMES=(`cat batch43.yml|yq .|jq '.studentDetails[].name'`)
+
+while [$STUDENT_COUNT -gt 0];do 
+
+echo -e "Hello ${NAME[$i]},\n\tMy name TRAINER_NAME and I am a COURSE_NAME 
 trainer.\nWe are starting a new batch from Date and timing is TIMING. If you are interested then please contact us on PH_NUMBER\n\n"
+STUDENT_COUNT=$(($STUDENT_COUNT-1))
+done    

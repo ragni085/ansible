036bd0a2c29e165efa73fe1d76f1a7152cd77b8e
Ragni kumari <ragni085@gmail.com>
2019-12-22 13:26:57 +0530

    some change

--- yaml _parser/script.sh
+++ yaml _parser/script.sh
@@ -8,15 +8,16 @@ fi
 i=0
 STUDENT_COUNT=$(cat batch43.yml | yq . | jq '.studentDetails[].name' | wc -l)
 NAMES=(`cat batch43.yml|yq .|jq '.studentDetails[].name'|xargs`)
-TRAINER_NAME=$(cat batch43.yml|yq .|jq '.tainerName')
-COURSENAME=$(cat batch43.yml|yq .|jq '.courseNmae')
-STARTDATE=$(cat batch43.yml|yq .|jq '.startDate')
-TIMING=$(cat batch43.yml|yq .|jq '.timing')
+#TRAINER_NAME=$(cat batch43.yml|yq .|jq '.tainerName')
+#COURSENAME=$(cat batch43.yml|yq .|jq '.courseNmae')
+#STARTDATE=$(cat batch43.yml|yq .|jq '.startDate')
+#TIMING=$(cat batch43.yml|yq .|jq '.timing')
 
 while [ $STUDENT_COUNT -gt 0 ]; do 
 
-echo e "Hello ${NAMES[$1]},\n\tMy name is ${TRAINER_NAME} and I am a ${COURSE_NAME} trainer.\nWe
-are starting a new batch from ${startDate} and timing is ${TIMING}.If you are interested then please contact us on PH_NUMBER\n\n"
+echo -e "Hello ${NAMES[$1]},\n\tMy name is TRAINER_NAME and I am a COURSE_NAME trainer.\nWe
+are starting a new batch from startDate and timing is TIMING.If you are interested
+then please contact us on PH_NUMBER\n\n"
 STUDENT_COUNT=$(($STUDENT_COUNT-1))
 i=$(($i+1))
 done      

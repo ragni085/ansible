b50ec0172916bad348cc85ccc4fc8bcad443d614
Ragni kumari <ragni085@gmail.com>
2019-12-22 12:33:33 +0530

    some change

--- yaml _parser/script.sh
+++ yaml _parser/script.sh
@@ -11,7 +11,8 @@ NAMES=(`cat batch43.yml|yq .|jq '.studentDetails[].name'`)
 
 while [$STUDENT_COUNT -gt 0];do 
 
-echo -e "Hello ${NAME[$i]},\n\tMy name TRAINER_NAME and I am a COURSE_NAME 
-trainer.\nWe are starting a new batch from Date and timing is TIMING. If you are interested then please contact us on PH_NUMBER\n\n"
+echo e "Hello XYZ,\n\tMy name is TRAINER_NAME and I am a COURSE_NAME trainer.
+ \nWe are starting a new batch from Date and timing is TIMING.
+ If you are interested then please contact us on PH_NUMBER\n\n"
 STUDENT_COUNT=$(($STUDENT_COUNT-1))
-done    
+done      

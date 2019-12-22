25a1a5f68dbeaf520a303ee6570663d2303e6c01
Ragni kumari <ragni085@gmail.com>
2019-12-22 13:16:13 +0530

    some change

--- yaml _parser/script.sh
+++ yaml _parser/script.sh
@@ -15,9 +15,8 @@ TIMING=$(cat batch43.yml|yq .|jq '.timing')
 
 while [ $STUDENT_COUNT -gt 0 ]; do 
 
-echo e "Hello XYZ,\n\tMy name is TRAINER_NAME and I am a COURSE_NAME trainer.
- \nWe are starting a new batch from Date and timing is TIMING.
- If you are interested then please contact us on PH_NUMBER\n\n"
+echo e "Hello XYZ,\n\tMy name is TRAINER_NAME and I am a COURSE_NAME trainer.\nWe
+are starting a new batch from Date and timing is TIMING.If you are interested then please contact us on PH_NUMBER\n\n"
 STUDENT_COUNT=$(($STUDENT_COUNT-1))
 i=$(($i+1))
 done      

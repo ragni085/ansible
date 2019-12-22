049ff324ef7163ee8e68dc8512db6c5574c4e637
Ragni kumari <ragni085@gmail.com>
2019-12-22 12:46:52 +0530

    some change

--- yaml _parser/script.sh
+++ yaml _parser/script.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
+ 
 if [! -f "$1"]; then
  echo "Given file doesnot exist"
  exit 1
 fi
 
 i=0
-STUDENT_COUNT=$(batch43.yml | yq . | jq '.studentDetails[].name' | wc -l)
-NAMES=(`cat batch43.yml|yq .|jq '.studentDetails[].name'`)
+STUDENT_COUNT=$(cat batch43.yml | yq . | jq '.studentDetails[].name' | wc -l)
+NAMES=(`cat batch43.yml|yq .|jq '.studentDetails[].name'|xargs`)
+TRAINER_NAME=(cat batch43.yml|yq .|jq '.tainerName')
+COURSENAME=(cat batch43.yml|yq .|jq '.courseNmae')
+STARTDATE=(cat batch43.yml|yq .|jq '.startDate')
+TIMING=(cat batch43.yml|yq .|jq '.timing')
 
 while [$STUDENT_COUNT -gt 0];do 
 
@@ -15,4 +19,5 @@ echo e "Hello XYZ,\n\tMy name is TRAINER_NAME and I am a COURSE_NAME trainer.
  \nWe are starting a new batch from Date and timing is TIMING.
  If you are interested then please contact us on PH_NUMBER\n\n"
 STUDENT_COUNT=$(($STUDENT_COUNT-1))
+i=$(($i+1))
 done      

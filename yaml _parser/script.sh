098c478372658c5b53b9208179924c29111b77ec
Ragni kumari <ragni085@gmail.com>
2019-12-25 00:19:45 +0530

    some change

--- yaml _parser/script.sh
+++ yaml _parser/script.sh
@@ -8,7 +8,10 @@ fi
 i=0
 STUDENT_COUNT=$(cat batch43.yml | yq . | jq '.studentDetails[].name' | wc -l)
 NAMES=(`cat batch43.yml|yq .|jq '.studentDetails[].name'|xargs`)
-
+TRAINER_NAME=$(cat batch43.yml|yq .|jq '.trainerName')
+COURSENAME=$(cat batch43.yml|yq .|jq '.courseNmae')
+STARTDATE=$(cat batch43.yml|yq .|jq '.startDate')
+TIMING=$(cat batch43.yml|yq .|jq '.timing')
 
 while [ $STUDENT_COUNT -gt 0 ]; do 
 
